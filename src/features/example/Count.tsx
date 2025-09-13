c7f3300e44ca5fe64ddf9131c4975cc49bd35cd9
waritboonmasiri <warit.boonmasiri@gmail.com>
2025-09-13 12:30:12 +1000

    Try count, add anime

---
+++ src/features/example/Count.tsx
@@ -0,0 +1,22 @@
+import { useMemo } from "react";
+import { Observer } from "mobx-react-lite";
+
+import CountModel from './CountModel';
+import { CountView } from "./CountView";
+
+
+export function Count() {
+    const model = useMemo(() => new CountModel(), []);
+
+    return (
+        <Observer>
+            {() => (
+                <CountView
+                    count={model.count}
+                    increment={() => model.increment()}
+                    decrement={() => model.decrement()}
+                />
+            )}
+        </Observer>
+    )
+};
---
+++ src/features/example/CountModel.ts
@@ -0,0 +1,19 @@
+import { makeAutoObservable } from "mobx";
+
+export default class CountModel {
+
+    constructor() {
+        makeAutoObservable(this)
+    }
+
+    count = 0;
+
+    increment() {
+        this.count += 1;
+    }
+
+    decrement() {
+        this.count -= 1;
+    }
+}
+
